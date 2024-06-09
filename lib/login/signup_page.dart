1dc27ad0d62d3d82cac17d61e29b610ea38ea842
Ameen PC <ameenpc135@gmail.com>
2024-06-09 20:50:56 +0530

    project

---
+++ lib/login/signup_page.dart
@@ -0,0 +1,264 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:erevive/login/loginpage.dart';
+import 'package:flutter/material.dart';
+import 'package:erevive/login/welcomemyapp.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SignupPage extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            height: MediaQuery.of(context).size.height - 50,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    FadeInUp(
+                      duration: const Duration(milliseconds: 1000),
+                      child: const Text(
+                        "Sign up",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    FadeInUp(
+                      duration: const Duration(milliseconds: 1200),
+                      child: Text(
+                        "Create an account, It's free",
+                        style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    FadeInUp(
+                      duration: const Duration(milliseconds: 1200),
+                      child: makeInput(
+                        label: "Name",
+                        controller: _nameController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This field is mandatory';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    FadeInUp(
+                      duration: const Duration(milliseconds: 1300),
+                      child: makeInput(
+                        label: "Phone Number",
+                        controller: _phoneNumberController,
+                        keyboardType: TextInputType.phone,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This field is mandatory';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    FadeInUp(
+                      duration: const Duration(milliseconds: 1400),
+                      child: makeInput(
+                        label: "Email",
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This field is mandatory';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    FadeInUp(
+                      duration: const Duration(milliseconds: 1500),
+                      child: makeInput(
+                        label: "Password",
+                        controller: _passwordController,
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This field is mandatory';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                FadeInUp(
+                  duration: const Duration(milliseconds: 1500),
+                  child: Container(
+                    padding: const EdgeInsets.only(top: 1, left: 1),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      border: const Border(
+                        bottom: BorderSide(color: Colors.black),
+                        top: BorderSide(color: Colors.black),
+                        left: BorderSide(color: Colors.black),
+                        right: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60,
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          try {
+                            await FirebaseAuth.instance
+                                .createUserWithEmailAndPassword(
+                              email: _emailController.text,
+                              password: _passwordController.text,
+                            );
+                            // Sign-up successful, navigate to next screen
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const MyApp(),
+                              ),
+                            );
+                          } catch (e) {
+                            if (e is FirebaseAuthException &&
+                                e.code == 'email-already-in-use') {
+                              // Handle case where email is already in use
+                              // Prompt the user to choose a different email or log in
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text(
+                                      'The email address is already in use. Please log in instead.'),
+                                  duration: Duration(seconds: 5),
+                                ),
+                              );
+                            } else {
+                              // Handle other errors
+                              print(e.toString());
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text(
+                                      'An error occurred. Please try again later.'),
+                                  duration: Duration(seconds: 5),
+                                ),
+                              );
+                            }
+                          }
+                        }
+                      },
+                      color: Colors.greenAccent,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: const Text(
+                        "Sign up",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 18),
+                      ),
+                    ),
+                  ),
+                ),
+                FadeInUp(
+                  duration: const Duration(milliseconds: 1600),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text("Already have an account?"),
+                      GestureDetector(
+                        onTap: () {
+                          // Navigate to login page
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginPage(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          " Login",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600, fontSize: 18),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget makeInput(
+      {label,
+      obscureText = false,
+      TextEditingController? controller,
+      required FormFieldValidator<String> validator,
+      TextInputType keyboardType = TextInputType.text}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          label,
+          style: const TextStyle(
+              fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        TextFormField(
+          controller: controller,
+          keyboardType: keyboardType,
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey.shade400)),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey.shade400)),
+          ),
+          validator: validator,
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+      ],
+    );
+  }
+}
